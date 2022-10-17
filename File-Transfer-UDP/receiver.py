9c9c1bd148cb79e7a5bafb634fcb0da0328d3198
João Paulo Cardoso <joaopaulocs09@gmail.com>
2022-10-17 10:02:28 -0300

    UDP  enviando e recebendo

---
+++ File-Transfer-UDP/receiver.py
@@ -0,0 +1,69 @@
+from select import select
+import socket
+import sys
+from time import sleep, time
+import os
+import locale
+import select
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+HOST = ''
+PORT = 1240
+BUFFER = 1000
+
+SEPARATOR = "<SEPARATOR>"
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+class UDPFileTransfer:
+
+    def initialConfigs():
+        global HOST, PORT, udp, BUFFER
+       
+        if len(sys.argv) == 3:
+            HOST = sys.argv[1]
+            PORT = int(sys.argv[2])
+        else:
+            print("Erro: adicionar parametros na execução do programa  ")
+            exit(1)
+
+        try:
+            udp.bind((HOST, PORT))
+
+            data, addr = udp.recvfrom(1024)
+           
+            file = ''
+            size = 0
+            BUFFER = 1000
+            if data:
+                file = data.decode('ascii')
+                print(f"Arquivo: {file}")
+            data, addr = udp.recvfrom(1024)
+            BUFFER = int(data.decode('ascii'))
+            data, addr = udp.recvfrom(1024)
+            size = int(data.decode('ascii'))
+
+            if os.path.exists(file):
+                os.remove(file)
+
+            print(size)
+            arq = open(file, "wb")
+            start = time()
+
+            while True:
+                ready = select.select([udp], [], [], 3)
+                if ready[0]:
+                    data, addr =  udp.recvfrom(BUFFER)
+                    arq.write(data)
+                else:
+                    break
+            
+
+            formatedSize = locale.format_string('%.3f',arq.tell(), grouping=True)
+            print("Tamanho do arquivo recebido: ", formatedSize)
+            print("Numero de pacotes recebidos: ", arq.tell() // BUFFER + 1)
+            
+        except Exception as e:
+            pass
+
+UDPFileTransfer.initialConfigs()
---
+++ File-Transfer-UDP/sender.py
@@ -0,0 +1,68 @@
+import socket
+import sys
+from time import sleep, time
+import os
+import locale
+
+HOST = ''
+PORT = 1240
+BUFFER = 1000
+
+SEPARATOR = "<SEPARATOR>"
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+class UDPFileTransfer:
+
+    def initialConfigs():
+        global HOST, PORT, udp, BUFFER
+       
+        if len(sys.argv) == 3:
+            HOST = sys.argv[1]
+            PORT = int(sys.argv[2])
+        else:
+            print("Erro: adicionar parametros na execução do programa  ")
+            exit(1)
+
+        Option = int(input('Selecione buffer \n1 - 1000 \n2 - 1500\n'))
+        if Option == 2:
+            BUFFER = 1500
+        elif Option != 1:
+            print("Opção inválida!")
+            exit()
+
+
+        print("Host: ",HOST, ",Porta: ", PORT, ',Buffer: ', BUFFER)
+
+        try: 
+            addr = (HOST, PORT)
+
+            file = input("Insira o arquivo a ser enviado: ")
+            size = os.path.getsize(file)
+
+            udp.sendto(str(os.path.basename(file)).encode('ascii'), addr)
+            sleep(0.5)
+            udp.sendto(str(BUFFER).encode('ascii'), addr)
+            sleep(0.5)
+            udp.sendto(str(size).encode('ascii'), addr)
+
+            cont = 0
+
+            with open(file, "rb") as arq:
+                start = time()
+                packageToSend = arq.read(BUFFER)
+                while packageToSend:
+                    if udp.sendto(packageToSend, addr):
+                        packageToSend = arq.read(BUFFER)
+                        cont += 1
+                    if cont == 2:
+                        sleep(0.05)
+                        cont = 0
+            
+        except Exception as e:
+            print(e)
+            return
+
+udpFileTransfer = UDPFileTransfer()
+
+UDPFileTransfer.initialConfigs()
